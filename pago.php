5c260c5ced16e86aa19a93f45b665b18f4182dc1
cam1la444 <camila24melara@gmail.com>
2024-10-07 06:32:14 -0600

    se agg la pag Pago.php

--- pago.php
+++ pago.php
@@ -112,11 +112,13 @@ if($productos != null){
             </table>
         </div>
 
-        <div class="row">
-            <div class="col-md-5 offset-md-7 d-grid gap-2">
-                <button class="btn btn-primary btn-lg">Realizar el pago</button>
+        <?php if($productos != null){ ?>
+            <div class="row">
+                <div class="col-md-5 offset-md-7 d-grid gap-2">
+                    <a href="pago.php" class="btn btn-primary btn-lg">Realizar el pago</a>
             </div>
         </div>
+        <?php } ?>
     </div>
 </main>
 
